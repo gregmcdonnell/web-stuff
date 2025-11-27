b426856b77d8ac9fad29bf03886d79cd72c14507
Greg McDonnell <146052605+gregmcdonnell@users.noreply.github.com>
2025-11-27 12:57:36 -0600

    Remove typo from main

--- Traffic/main.js
+++ Traffic/main.js
@@ -53,7 +53,7 @@ function createGUI() {
 
     gui.add(trafficSim, 'acceleration', 0.1, 10.0, 0.1).name('Acceleration');
     gui.add(trafficSim, 'stopping', 0.1, 10.0, 0.1).name('Stopping');
-u
+
 }
 createGUI();
 
